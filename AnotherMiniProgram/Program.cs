692ddcaff78faecd99933e400cb9921b7f7f05bc
chajus1 <chajus1@googlemail.com>
2023-04-29 16:51:30 +0100

    Add project files.

---
+++ AnotherMiniProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherMiniProgram
+{
+    public enum ProductCodes
+    {
+        Milk = 0,
+        Juice = 1,
+        Tea = 2,
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ProductCodes test = ProductCodes.Tea;
+            Console.WriteLine((int)test);
+
+            int test2 = (int)ProductCodes.Juice;
+
+            Console.WriteLine((ProductCodes)test2);
+        }
+    }
+}
